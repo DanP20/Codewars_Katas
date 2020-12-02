874ced96a8baa6dea948bc26856004136b141364
danp20 <dan.parry22@gmail.com>
2020-12-02 13:16:29 +0100

    num to string & back

---
+++ 7kyu/Form the largest/test.js
@@ -0,0 +1,7 @@
+describe("Basic tests",_=>{
+    Test.assertEquals(maxNumber(213), 321);
+    Test.assertEquals(maxNumber(7389), 9873);
+    Test.assertEquals(maxNumber(63792), 97632);
+    Test.assertEquals(maxNumber(566797), 977665);
+    Test.assertEquals(maxNumber(1000000), 1000000);
+  });
